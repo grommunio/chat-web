755d52db919cb80ed88debf2d69f1b11cff0e121
Victor Tesla <victor.tesla@grammm.com>
2021-05-13 13:20:37 +0200

    update about modal dialog

--- components/about_build_modal/about_build_modal.tsx
+++ components/about_build_modal/about_build_modal.tsx
@@ -97,14 +97,14 @@ export default class AboutBuildModal extends React.PureComponent<Props, State> {
             <div>
                 <FormattedMessage
                     id='about.teamEditionLearn'
-                    defaultMessage='Join the Mattermost community at '
+                    defaultMessage='Join the grommunio community at '
                 />
                 <a
                     target='_blank'
                     rel='noopener noreferrer'
-                    href='http://www.mattermost.org/'
+                    href='http://grommunio.com/'
                 >
-                    {'mattermost.org'}
+                    {'grommunio.com'}
                 </a>
             </div>
         );
@@ -223,7 +223,7 @@ export default class AboutBuildModal extends React.PureComponent<Props, State> {
                     >
                         <FormattedMessage
                             id='about.title'
-                            values={{appTitle: config.SiteName || 'Mattermost'}}
+                            values={{appTitle: config.SiteName || 'grommunio chat'}}
                             defaultMessage='About {appTitle}'
                         />
                     </Modal.Title>
@@ -234,15 +234,13 @@ export default class AboutBuildModal extends React.PureComponent<Props, State> {
                             <MattermostLogo/>
                         </div>
                         <div>
-                            <h3 className='about-modal__title'>
-                                <strong>{'Mattermost'} {title}</strong>
-                            </h3>
+                            <h3 className='about-modal__title'>{'grommunio chat'}</h3>
                             <p className='about-modal__subtitle pb-2'>{subTitle}</p>
                             <div className='form-group less'>
                                 <div>
                                     <FormattedMessage
                                         id='about.version'
-                                        defaultMessage='Mattermost Version:'
+                                        defaultMessage='grommunio chat Version:'
                                     />
                                     <span id='versionString'>{'\u00a0' + mmversion}</span>
                                 </div>
@@ -288,7 +286,7 @@ export default class AboutBuildModal extends React.PureComponent<Props, State> {
                         <p>
                             <FormattedMarkdownMessage
                                 id='about.notice'
-                                defaultMessage='Mattermost is made possible by the open source software used in our [server](!https://about.mattermost.com/platform-notice-txt/), [desktop](!https://about.mattermost.com/desktop-notice-txt/) and [mobile](!https://about.mattermost.com/mobile-notice-txt/) apps.'
+                                defaultMessage='Mattermost is made possible by the open source software used in our [server](!https://docs.grommunio.com/chat/), [desktop](!https://docs.grommunio.com/chat/) and [mobile](!https://docs.grommunio.com/chat/) apps.'
                             />
                         </p>
                     </div>
